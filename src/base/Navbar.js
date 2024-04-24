d8d6921eafa7e3c1abedb64a12e3ec55b2418b22
vikikrivich <levigowik@gmail.com>
2024-04-24 16:51:15 +0600

    init

---
+++ src/base/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Viki To-Do list</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New To-Do</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
